62819b4c333578882f4a80b5b59e59970cefd5d3
Joseph Macharia <josephm2800@gmail.com>
2022-05-05 07:30:29 +0300

    eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 const nextConfig = {
   reactStrictMode: true,
 }
+
 
 module.exports = nextConfig
